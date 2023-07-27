74a31c6f1527aac06762ee5d1e70c7e48de06491
Kamil Kolenda <kamil.kolenda@ideamotive.com>
2023-07-27 11:19:15 +0200

    Fix port

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+const port = process.env.PORT || 3000;
 const express = require("express");
 const app = express();
-app.get("/index.html", function (req, res) {
+app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
 });
-app.listen(3000, function () {
+app.get("/paczkomaty.html", function (req, res) {
+    res.sendFile(__dirname + "/paczkomaty.html");
+});
+app.listen(port, function () {
     console.log("Server is running on localhost3000");
 });
